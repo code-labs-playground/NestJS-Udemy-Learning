d7dbebbdfa415ff56033815cc8deab551f98ea3c
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-07-16 14:09:14 +0530

    Creating the Dependency Injection Repository

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksRepository } from './tasks.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TasksRepository]), // Importing the TasksRepository (Using Dependency Injection to access the repository in the service)
+  ],
+  exports: [],
   controllers: [TasksController],
   providers: [TasksService],
 })
