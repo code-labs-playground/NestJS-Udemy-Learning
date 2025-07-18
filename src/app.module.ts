33ec640ac30a3734fbae88ef25c273d8c6fcee23
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-07-18 10:53:20 +0530

    Setuping AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,16 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true, // Automatically load entities from imported modules
       synchronize: true, // Note: Set to false in production
     }),
-    // TypeOrmModule.forRootAsync({
-    //   useFactory: () => ({
-    //     type: 'postgres',
-    //     host: process.env.DB_HOST,
-    //     port: parseInt(process.env.DB_PORT, 10) || 5432,
-    //     username: process.env.DB_USERNAME,
-    //     password: process.env.DB_PASSWORD,
-    //     database: process.env.DB_DATABASE,
-    //   }),
-    // }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
