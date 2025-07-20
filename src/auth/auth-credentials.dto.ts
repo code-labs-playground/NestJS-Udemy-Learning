039f8037895de056f17ae7bae036659fa3df285f
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-07-20 12:12:50 +0530

    Validations done

--- src/auth/auth-credentials.dto.ts
+++ src/auth/auth-credentials.dto.ts
@@ -1,4 +1,25 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4, { message: 'Username must be at least 4 characters long' })
+  @MaxLength(20, { message: 'Username must not exceed 20 characters' })
+  @IsNotEmpty({ message: 'Username is required' })
   username: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @MaxLength(20, { message: 'Password must not exceed 20 characters' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number or special character',
+  })
   password: string;
 }
