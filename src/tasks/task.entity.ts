c90377ae54b415fd82d99d0e9cda0e5b9a06f260
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-07-23 11:42:01 +0530

    Update

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 import { IsEnum } from 'class-validator';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 import { Exclude } from 'class-transformer';
 
 @Entity()
--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -4,7 +4,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 
 @Injectable()
 export class TasksRepository extends Repository<Task> {
