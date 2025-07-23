460d01e163ce52136a790d6d6b0e8e8c870c0c39
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-07-23 10:22:13 +0530

    coupling the env files using ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,30 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Makes ConfigModule globally available
-      envFilePath: ['.env'], // Load environment variables from .env file
+      envFilePath: [`.env.stage.${process.env.STAGE}`], // Load environment variables from .env file
     }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as 'postgres', // Database type
-      host: process.env.DB_HOST ?? 'localhost', // Database host
-      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432, // Database port
-      username: process.env.DB_USERNAME, // Database username
-      password: process.env.DB_PASSWORD, // Database password
-      database: process.env.DB_DATABASE, // Database name
-      autoLoadEntities: true, // Automatically load entities from imported modules
-      synchronize: true, // Note: Set to false in production
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Import ConfigModule to access environment variables
+      inject: [ConfigService], // Inject ConfigService to access configuration
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get('DB_TYPE') as 'postgres', // Database type
+          host: configService.get('DB_HOST') ?? 'localhost', // Database host
+          port: parseInt(configService.get<string>('DB_PORT') ?? '5432', 10), // Database port
+          username: configService.get('DB_USERNAME'), // Database username
+          password: configService.get('DB_PASSWORD'), // Database password
+          database: configService.get<string>('DB_DATABASE'), // Database name
+          autoLoadEntities: true, // Automatically load entities from imported modules
+          synchronize: true, // Note: Set to false in production
+        };
+      },
     }),
     AuthModule,
   ],
